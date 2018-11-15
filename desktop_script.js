0af937cd43b0c5178d2a328cc841871e403a6bc2
Autowin <luongthanhkx1997@gmail.com>
2018-11-15 20:46:42 +0700

    temparory

--- desktop_script.js
+++
@@ -1,7 +0,0 @@
-var fs=require('fs');
-var data=fs.readFileSync('test.json', 'utf8');
-var words=JSON.parse(data);
-console.log(words);
-console.log(words.length);
-console.log(words[0].MSV);
-console.log(words[0].idle1.TC);
--- readFileJson.js
+++
@@ -1,4 +0,0 @@
-var fs=require('fs');
-var data=fs.readFileSync('test.json', 'utf8');
-var words=JSON.parse(data);
-console.log(words);
